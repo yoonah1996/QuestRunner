4114d6dedd06e11e1d6cfd6bff1a3db84f44d875
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-04-23 15:32:41 +0900

    :heavy_plus_sign: added react-router-dom dependency, :bento: react view route setting complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { RootState } from './store/modules';
 // 라우팅
 // Islogin
-
-
-function App() {
-  const isLogin = useSelector((state:RootState) => state.userSetter.isLogin);
+const App: React.FC = () => {
+  const isLogin = useSelector((state: RootState) => state.userSetter.isLogin);
   return (
     <div className="App">
+      <Switch>
+        <Route
+          exact
+          path="/userLoginPage"
+          render={() => <UserLogin/>}
+        />
+        <Route
+          exact
+          path="/userJoinPage"
+          render={() => <UserJoin/>}
+        />
+        <Route
+          path="/user"
+          render={() => {
+            if (!isLogin) {
+              return <Redirect to="/userLoginPage" />;
+            }
+            return <Theplace />;
+          }}
+        />
+        <Route path="/" render={() => <Redirect to="/user" />} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
