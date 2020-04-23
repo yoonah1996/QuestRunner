1a7920e7dc37056f8853c6e648ca03f0b2292af5
kimhwan <khan@MacBook-Pro.local>
2020-04-23 14:19:41 +0900

    :boom: redux store not work!!!

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './store/modules'
 
-
+const store = createStore(rootReducer);
+console.log(store.getState());
 ReactDOM.render(
+    <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
