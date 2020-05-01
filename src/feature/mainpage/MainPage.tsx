b4c3d34457b741c7cb1861fd930fc3003bae20c9
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-05-01 11:08:30 +0900

    :lipstick: added mainpage

--- src/feature/mainpage/MainPage.tsx
+++ src/feature/mainpage/MainPage.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import {
+  Theme, createStyles, makeStyles, useTheme,
+} from '@material-ui/core/styles';
 import { RouteComponentProps } from 'react-router-dom';
 import NavBar from '../navbar/NavBar';
 import ExBarAvatar from '../experiencebar/ExBarAvatar';
 import ExperienceBar from '../experiencebar/ExperienceBar';
 
+import Image from '../../img/jungle.jpg';
 
-const MainPage: React.FC<RouteComponentProps> = () => (
-  <Grid
-    container
-    direction="column"
-    justify="flex-end"
-    alignItems="stretch"
-  >
-    <NavBar />
-    <ExBarAvatar />
-    <ExperienceBar />
-  </Grid>
-);
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  bg: {
+    backgroundImage: `url(${Image})`,
+  },
+}));
+
+
+const MainPage: React.FC<RouteComponentProps> = () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      className={classes.bg}
+      container
+      direction="column"
+      justify="flex-end"
+      alignItems="stretch"
+    >
+      <NavBar />
+      <ExBarAvatar />
+      <ExperienceBar />
+    </Grid>
+  );
+};
 export default MainPage;
