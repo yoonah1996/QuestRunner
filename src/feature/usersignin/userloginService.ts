fe84ef8e989fc491f744c773a17c1fac42766d2c
kimhwan <khan@MacBook-Pro.local>
2020-05-07 12:40:48 +0900

    :ok_hand: store done

--- src/feature/usersignin/userloginService.ts
+++ src/feature/usersignin/userloginService.ts
@@ -1,15 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
-import {
-  createAction, createSlice, PayloadAction,
-} from '@reduxjs/toolkit';
+import { createAction, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '../common/interfaces';
 
 type UserState = {
-  user:User | null;
-  isLogin:boolean;
-  accessToken:string | null;
-  refreshToken:string | null;
+  user: User | null;
+  isLogin: boolean;
+  accessToken: string | null;
+  refreshToken: string | null;
 };
 
 const initialState: UserState = {
@@ -25,12 +23,17 @@ const setLogin = createAction<object>(`${actionPrefix}/login`);
 const setUser = createAction<object>(`${actionPrefix}/user`);
 
 const reducers = {
-  login: (state : UserState, { payload: { isLogin, accessToken, refreshToken } } : PayloadAction<UserState>) => {
+  login: (
+    state: UserState,
+    {
+      payload: { isLogin, accessToken, refreshToken },
+    }: PayloadAction<UserState>,
+  ) => {
     state.isLogin = isLogin;
     state.accessToken = accessToken;
     state.refreshToken = refreshToken;
   },
-  user: (state : UserState, { payload: { user } }:PayloadAction<UserState>) => {
+  user: (state: UserState, { payload: { user } }: PayloadAction<UserState>) => {
     state.user = user;
   },
 };
