838bca5651769d06f212d2330c2dab160dad4a2d
yoonah1996 <yoonah1996@gmail.com>
2020-05-08 18:46:01 +0900

    :lipstick: rankImg add

--- src/feature/rank/TopThree.tsx
+++ src/feature/rank/TopThree.tsx
@@ -1,11 +1,24 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Grid, Paper } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import AWS from 'aws-sdk';
 import RunnerA from '../../img/runnerA.gif';
+// const { accessKeyId, secretAccessKey, bucketName } = process.env;
 
+const accessKeyId = 'AKIAJETOR3EJQUYUCD6A';
+const secretAccessKey = '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';
+const bucketName = 'qrunner-avatar';
+
+const s3 = new AWS.S3({
+  accessKeyId,
+  secretAccessKey,
+  useAccelerateEndpoint: false,
+});
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -19,7 +32,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     padding: theme.spacing(2),
   },
   image: {
-    height: 'auto',
+    height: 230,
     width: 200,
     display: 'block',
     marginLeft: 'auto',
@@ -38,10 +51,34 @@ interface threetype {
 
 const TopThree: React.FC<threetype> = (props) => {
   const classes = useStyles();
+  const [value, setValue] = React.useState('');
+
+  const getRankTop = async () => {
+    console.log(props);
+    const params: any = {
+      Bucket: bucketName,
+      Key: props._id,
+    };
+    s3.getObject(params, (err: AWS.AWSError, data: any) => {
+      if (err) {
+        return '';
+      }
+      console.log(data);
+      const blob = new Blob([data.Body], { type: data.ContentType });
+      const blobUrl = URL.createObjectURL(blob);
+      setValue(blobUrl);
+    });
+  };
+
+  useEffect(() => {
+    getRankTop();
+  }, [props]);
 
   return (
     <div>
-      <img className={classes.image} src={RunnerA} alt="" />
+      {!props.profilePic ?
+        <img className={classes.image} src={RunnerA} alt="" />
+        : <img className={classes.image} src={value} alt="" />}
       {!props.username ? null : (
         <div>
           {props.username}
