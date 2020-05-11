179586673cf7ab3a3657f7f63065d34816f913c6
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-05-12 02:36:56 +0900

    :ok_hand: quest

--- src/feature/quest/Quest.tsx
+++ src/feature/quest/Quest.tsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import List from '@material-ui/core/List';
 import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
@@ -14,25 +21,73 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import Divider from '@material-ui/core/Divider';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
 import QuestEntry from './QuestEntry';
+import { RootState } from '../index';
+import { serverHttp } from '../common/utils';
+import { userLoginActions } from '../usersignin/userloginService';
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   button: {
     margin: theme.spacing(1),
+    position: 'absolute',
+    top: -50,
+    left: 700,
+  },
+  list: {
+    position: 'absolute',
+    top: 250,
   },
   root: {
     flexGrow: 1,
-    maxWidth: 1000,
+    maxWidth: 500,
   },
   demo: {
     backgroundColor: theme.palette.background.paper,
   },
+  main: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 300,
+  },
 }));
 
 export default function Quest() {
+  const dispatch = useDispatch();
   const [open, setOpen] = React.useState(false);
+  const [toast, setToast] = React.useState(false);
+  const [title, setTitle] = React.useState<string | null>('');
+  const [content, setContent] = React.useState<string | null>('');
+  const today = new Date();
+  const accessToken = useSelector((state : RootState) => state.userLogin.accessToken);
+  const quests = useSelector((state : RootState) => state.userLogin.user?.quests);
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+    today,
+  );
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setContent(event.target.value);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -41,15 +96,54 @@ export default function Quest() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleToastClick = () => {
+    setToast(true);
+  };
+
+  const handleToastClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setToast(false);
+  };
+
+  const handleAddQuest = () => {
+    axios({
+      method: 'post',
+      url: `${serverHttp}/quest`,
+      headers: {
+        Authorization: accessToken,
+      },
+      data: {
+        title,
+        content,
+        createdAt: today,
+        dueDate: selectedDate,
+      },
+    })
+      .then(() => {
+        axios.get(`${serverHttp}/userinfo`, {
+          headers: {
+            Authorization: accessToken,
+          },
+        })
+          .then((response) => {
+            dispatch(userLoginActions.setUser({ user: response.data }));
+            handleToastClick();
+            handleClose();
+          });
+      })
+      .catch((response) => {
+        // handle error
+        console.log(response);
+      });
+  };
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="stretch"
-    >
+    <div className={classes.main}>
       <Button
         variant="contained"
         color="secondary"
@@ -60,39 +154,76 @@ export default function Quest() {
         ADD
       </Button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">add quest</DialogTitle>
+        <DialogTitle id="form-dialog-title">Quest Register</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            퀘스트 추가 폼 작성예정
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="title"
-            label="Title"
-            type="text"
-            fullWidth
-          />
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="stretch"
+          >
+            <TextField
+              autoFocus
+              margin="dense"
+              id="title"
+              label="Title"
+              type="text"
+              value={title}
+              onChange={handleTitleChange}
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <KeyboardDatePicker
+                disableToolbar
+                variant="inline"
+                format="yyyy/MM/dd"
+                margin="normal"
+                minDate={today}
+                id="date-picker-inline"
+                label="Due Date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+            </MuiPickersUtilsProvider>
+            <TextField
+              id="outlined-multiline-static"
+              label="Quest Content"
+              multiline
+              rows={6}
+              variant="outlined"
+              value={content}
+              onChange={handleContentChange}
+            />
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleAddQuest} color="primary">
             Add
           </Button>
         </DialogActions>
       </Dialog>
+      <Snackbar open={toast} autoHideDuration={6000} onClose={handleToastClose}>
+        <Alert onClose={handleToastClose} severity="success">
+          퀘스트등록완료!
+        </Alert>
+      </Snackbar>
       <div className={classes.root}>
         <div className={classes.demo}>
           <List>
-            <QuestEntry />
-            <QuestEntry />
-            <QuestEntry />
-            <QuestEntry />
+            {quests?.map((val) => (
+              <div>
+                <QuestEntry quest={val} />
+                <Divider variant="inset" component="li" />
+              </div>
+            ))}
           </List>
         </div>
       </div>
-    </Grid>
+    </div>
   );
 }
--- src/feature/quest/QuestEntry.tsx
+++ src/feature/quest/QuestEntry.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
@@ -11,30 +13,90 @@ import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import ConfirmationNumberIcon from '@material-ui/icons/ConfirmationNumber';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DetailsIcon from '@material-ui/icons/Details';
+import Typography from '@material-ui/core/Typography';
+import { QuestItem } from '../common/interfaces';
 
-export default function QuestEntry() {
+interface IProps {
+  quest: QuestItem | null
+}
+
+
+export default function QuestEntry({ quest } : IProps) {
+  const [checked, setChecked] = React.useState(quest?.checked);
+  const [questopen, setQuestOpen] = React.useState(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+  const handleClickOpen = () => {
+    setQuestOpen(true);
+  };
+
+  const handleClose = () => {
+    console.log("click");
+    setQuestOpen(false);
+  };
+
   return (
-    <ListItem button>
+    <ListItem>
       <ListItemAvatar>
-        <Avatar>
-          <KeyboardArrowDownIcon />
+        <Avatar onClick={handleClickOpen}>
+          <DetailsIcon />
         </Avatar>
       </ListItemAvatar>
       <ListItemText
-        primary="Single-line item"
-        secondary="Secondary text"
+        primary={quest?.title}
       />
       <ListItemSecondaryAction>
         <Checkbox
           edge="end"
+          checked={checked}
+          onChange={handleChange}
         />
-        <IconButton edge="end" aria-label="delete">
-          <DeleteIcon />
-        </IconButton>
+        {checked && (
         <IconButton edge="end" aria-label="delete">
           <ConfirmationNumberIcon />
         </IconButton>
+        )}
+        {!checked && (
+        <IconButton edge="end" aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+        )}
       </ListItemSecondaryAction>
+      <Dialog open={questopen} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">{quest?.title}</DialogTitle>
+        <DialogContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Created Date
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {quest?.created_at}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Due Date
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {quest?.due_date}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            Content
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {quest?.contents}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     </ListItem>
   );
 }
